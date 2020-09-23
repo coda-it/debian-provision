a05bbd96cd84460f6b7b1acedc3d55ce5d2f3e8e
Oskar Szura <oskar.szura@gmail.com>
2020-09-23 18:51:29 +0200

    CODA-109 - Provision server for webapp

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ if [ ! -z "$WEBAPP_V"  ]
 then
         sudo docker pull oszura/webapp-prod:$WEBAPP_V
         sudo docker rm -f $(sudo docker ps -a -q)
-        sudo docker run -v /home/oskarszura/db:/data/db -d --network=host mongo
+        sudo docker run -v /root/db:/data/db -d --network=host mongo
         sudo docker run -e WEBAPP_HTTP_PORT=80 -d --network=host   oszura/webapp-prod:$WEBAPP_V
         curl -X POST -H "Content-type: application/json" --data "{\"text\":\"webapp $WEBAPP_V deployed\"}" "NOTIFICATION_URL"
 fi
--- install.sh
+++ install.sh
@@ -1,17 +1,28 @@
 # Install genetic tools
 sudo apt update
 sudo apt install git
-mkdir -p ~/gowork/src/github.com/coda-it
+
+# Install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
 
 # Install cear
+mkdir -p ~/gowork/src/github.com/coda-it/cear
 git clone https://github.com/coda-it/cear ~/gowork/src/github.com/coda-it
 (cd ~/gowork/src/github.com/coda-it/cear && make)
 
 cp ./cear.conf ~/gowork/src/github.com/coda-it/cear
 cp ./deploy.sh ~/gowork/src/github.com/coda-it/cear
-sudo sed -i "" "s/NOTIFICATION_URL.*/$(NOTIFICATION_URL)/g" ~/gowork/src/github.com/coda-it/cear/deploy.sh
+sudo sed -i "s/NOTIFICATION_URL/$NOTIFICATION_URL/g" ~/gowork/src/github.com/coda-it/cear/deploy.sh
 
 sudo cp ./cear.service /etc/systemd/system
-sudo sed -i "" "s/CEAR_KEY_VALUE.*/$(CEAR_KEY_VALUE)/g" /etc/systemd/system/cear.service
+sudo sed -i "s/CEAR_KEY_VALUE/$CEAR_KEY_VALUE/g" /etc/systemd/system/cear.service
 sudo systemctl daemon-reload
 sudo systemctl start cear.service
+
+# Create db space
+sudo mkdir -p /root/db
